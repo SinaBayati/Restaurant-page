a87c4ed36523faae1ccb680ec6daa58bbabc2539
Sina Bayati <bayati.sina.w@gmail.com>
2024-05-09 08:33:50 +0330

    Build the contact section

--- src/contact.js
+++ src/contact.js
@@ -2,7 +2,16 @@ export default function(){
   const content = document.querySelector("#content");
   content.innerHTML = null;
 
-  // render contact section code
+  const contactContainer = document.createElement("div");
+  contactContainer.classList.add("contact-container");
+
+  const title = document.createElement("h2");
+  title.textContent = "Address";
+
+  const address = document.createElement("p");
+  address.textContent = "234 Emerald Street, Golem Town, Overworld";
+
+  contactContainer.append(title,address);
 
   content.appendChild(contactContainer);
 };
