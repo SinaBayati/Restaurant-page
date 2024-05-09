4e01d98b570f6db18b436723f2b0df4b96276600
Sina Bayati <bayati.sina.w@gmail.com>
2024-05-09 04:46:44 +0330

    Set up initial project structure

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname,"dist"),
+    clean: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader","css-loader"],
+      },
+      {
+        test: /\.(png|svg|gif|jpg|jpeg)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(ttf|otf|eot|woff|woff2)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+};
